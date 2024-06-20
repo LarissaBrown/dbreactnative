2bbc1f40f61a66aa56ae15834cfbc864165db0d0
Lolly <larissabrowndeveloper@gmail.com>
2024-06-20 10:54:59 -0600

    update: update files removed repeated file

--- AwesomeProject/app/(tabs)/index.tsx
+++ AwesomeProject/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Image, StyleSheet, Platform } from 'react-native';
-import { HelloWave } from '../../components/HelloWave';
+import { SignIn } from '../../components/SignIn';
 import ParallaxScrollView from '../../components/ParallaxScrollView';
 import { ThemedText } from '../../components/ThemedText';
 import { ThemedView } from '../../components/ThemedView';
@@ -16,7 +16,7 @@ export default function HomeScreen() {
       }>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Doodle Base for Zuri</ThemedText>
-        <HelloWave />
+        <SignIn />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Find Your Doodle</ThemedText>
